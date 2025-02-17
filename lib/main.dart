bd975acfc511feab4d762ab46bd4c668b5fd8c98
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-02-18 03:18:37 +0500

    feat(app): Integrate SplashCubit into MyApp using BlocProvider
    Integrated `SplashCubit` into the `MyApp` widget in `lib/main.dart` using `BlocProvider` to manage the Splash Page state.
    
    This commit connects the state management logic of `SplashCubit` to the application's UI by:
    
    - **Wrapping `MaterialApp` with `BlocProvider`:**  Ensures that `SplashCubit` is available to the `SplashPage` and its descendants in the widget tree.
    - **Providing `SplashCubit` instance:**
        - Creates a `SplashCubit` instance using `create: (context) => SplashCubit()`.
        - Automatically starts the `SplashCubit`'s logic by calling `..appStarted()` immediately after creation, initiating the splash screen delay and state transitions.
    
    By integrating `SplashCubit` with `BlocProvider`, the application is now set up to utilize state management for the Splash Page, enabling dynamic UI updates and navigation based on the Splash Page state.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/core/theme/app_theme.dart';
+import 'package:movie_app/features/splash/presentation/cubit/splash_cubit.dart';
 import 'package:movie_app/features/splash/presentation/page/splash_page.dart';
 
 void main() {
@@ -12,13 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-    ));
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.appTheme,
-      home: const SplashPage(),
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+    return BlocProvider(
+      create: (context) => SplashCubit()..appStarted(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.appTheme,
+        home: const SplashPage(),
+      ),
     );
   }
 }
