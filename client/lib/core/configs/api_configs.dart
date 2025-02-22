67658b5131967991098bd0a29b114a9163833739
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-02-22 09:02:23 +0500

    chore(core-config): Update baseURL in ApiConfig to new backend endpoint
    Updated the `baseURL` constant in `lib/core/configs/api_configs.dart` to point to the new backend API endpoint: 'https://movieapp-production-3fa6.up.railway.app/'.
    
    This commit updates the application's API configuration by changing the base URL to the new backend endpoint.
    
    This change may be due to:
    - Deployment of the backend to a new hosting environment.
    - Switching to a different backend environment (e.g., staging, production).
    - Migration or update of the backend server infrastructure.
    
    Ensure that the new `baseURL` 'https://movieapp-production-3fa6.up.railway.app/' is the correct and intended endpoint for the application to communicate with the backend API.

--- client/lib/core/configs/api_configs.dart
+++ client/lib/core/configs/api_configs.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
 
-  static const baseURL = 'https://netflix-backend-production-8413.up.railway.app/'; 
+  static const baseURL = 'https://movieapp-production-3fa6.up.railway.app/'; 
   static const apiV = 'api/v1/';
   
   static const signup = '${apiV}auth/signup';
