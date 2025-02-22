7b933e7edb891d1608df8ab6ac9990ed7fba929a
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-02-22 10:09:39 +0500

    refactor(auth-repo): Implement Dependency Injection for AuthRepositoryImpl
    Implemented Dependency Injection in `AuthRepositoryImpl` by injecting `AuthRemoteSource` as a dependency.
    
    This commit enhances the design and testability of the `AuthRepositoryImpl` by:
    
    - **Injecting `AuthRemoteSource` dependency:**
        - Modified the constructor of `AuthRepositoryImpl` to accept `AuthRemoteSource` as a required parameter, enabling constructor injection.
        - Stored the injected `AuthRemoteSource` instance as a class member for later use.
    
    - **Using injected `AuthRemoteSource` in `signUpWithEmailAndPassword`:**
        - Updated `signUpWithEmailAndPassword` method to delegate the API call to the injected `authRemoteSource` instance, instead of a hardcoded or internally created dependency.
    
    This Dependency Injection refactoring improves:
    - **Testability:** Allows for easy mocking or stubbing of `AuthRemoteSource` in unit tests for `AuthRepositoryImpl`.
    - **Modularity:** Decouples `AuthRepositoryImpl` from concrete `AuthRemoteSource` implementations, increasing reusability and flexibility.
    - **Maintainability:**  Makes the code easier to understand and modify by clearly separating concerns and dependencies.
    - **Adherence to SOLID principles:**  Specifically, the Dependency Inversion Principle.
    
    This change makes the `AuthRepositoryImpl` more robust, testable, and aligned with best practices for software design and dependency management.

--- client/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ client/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,11 +1,19 @@
 import 'package:dartz/dartz.dart';
+
 import 'package:movie_app/features/auth/data/models/sign_up_request_params_model.dart';
+import 'package:movie_app/features/auth/data/sources/auth_remote_source.dart';
 import 'package:movie_app/features/auth/domain/repositories/auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
+
+  AuthRemoteSource authRemoteSource;
+  AuthRepositoryImpl({
+    required this.authRemoteSource,
+  });
+
   @override
-  Future<Either> signUpWithEmailAndPassword(SignUpRequestParamsModel params) {
-    throw UnimplementedError();
+  Future<Either> signUpWithEmailAndPassword(SignUpRequestParamsModel params) async {
+    return await authRemoteSource.signUpWithEmailAndPassword(params);
   } 
 
 }
