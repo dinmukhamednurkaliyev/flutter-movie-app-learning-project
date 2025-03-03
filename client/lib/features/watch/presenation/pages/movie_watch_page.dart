7e0c681d109044dca97857fb959f7e3e459d463e
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-04 01:21:30 +0500

    feat(movie_watch): Add VideoTitleWidget to display movie title in MovieWatchPage

--- client/lib/features/watch/presenation/pages/movie_watch_page.dart
+++ client/lib/features/watch/presenation/pages/movie_watch_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/core/widgets/app/app_bar_widget.dart';
 import 'package:movie_app/features/movie/domain/entities/movie_entity.dart';
 import 'package:movie_app/features/watch/presenation/widgets/video_player_widget.dart';
+import 'package:movie_app/features/watch/presenation/widgets/video_title_widget.dart';
 
 class MovieWatchPage extends StatelessWidget {
   final MovieEntity movieEntity;
@@ -13,7 +14,14 @@ class MovieWatchPage extends StatelessWidget {
       appBar: const AppbarWidget(hideBack: false),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(children: [VideoPlayerWidget(id: movieEntity.id!)]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            VideoPlayerWidget(id: movieEntity.id!),
+            const SizedBox(height: 20),
+            VideoTitleWidget(title: movieEntity.title!),
+          ],
+        ),
       ),
     );
   }
